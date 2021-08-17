4147c617ea630370d14aa83040e3b34df33ae152
Quest <loyolaquester@gmail.com>
2021-08-17 09:13:27 +0800

    Added Promises to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ alert("Hello"); //Testing if Javascript is functioning
 var btn = document.querySelector("button"); //Setting a Query Selector for the Button
 var counter = 1;
 
+
+//Function for If Else
 btn.addEventListener("click", function(){
     if(counter === 5){
         alert("Maximum 5 clicks are reached! Will reset the counter back to 0");
@@ -13,4 +15,14 @@ btn.addEventListener("click", function(){
     
 });
 
-
+//Function for Promises
+// btn.addEventListener("click", function(){
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1
+//     if (a == 2) {
+//         resolve(alert("Success"))
+//     } else {
+//         reject(alert("Failed"))
+//     }
+// })
+// });
